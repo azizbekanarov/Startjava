3ad56a7d841217b5693ebda3190e4b825e517de0
Aziz PeAkSoFt 2022 <azizbekanarov@gmail.com>
2022-03-02 16:18:29 +0300

    azziz

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code
+        int a=33;
+        in
+    }t b=43;
+
+            System.out.println("no hello");
+            for (int i = 0; i < a.length; i++) {
+
+            }
+        }
+    }
+
